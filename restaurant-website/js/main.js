2fb819b8006226c29409d1f22be8914ce97dbf8a
357JDL <357JDL@gmail.com>
2023-02-23 14:32:39 -0800

    Added 'about me', made restaurant website responsive, made rubiks website responsive

---
+++ restaurant-website/js/main.js
@@ -0,0 +1,111 @@
+document.querySelector("#pic-1").addEventListener('click', showPic1)
+document.querySelector("#pic-2").addEventListener('click', showPic2)
+document.querySelector("#pic-3").addEventListener('click', showPic3)
+document.querySelector("#pic-4").addEventListener('click', showPic4)
+document.querySelector("#pic-5").addEventListener('click', showPic5)
+document.querySelector("#pic-6").addEventListener('click', showPic6)
+document.querySelector("#pic-7").addEventListener('click', showPic7)
+document.querySelector("#pic-8").addEventListener('click', showPic8)
+
+function showPic1(){
+	document.querySelector("#pic1").style.display = "block";
+	document.querySelector("#pic1").style.height = "100%";
+	// document.querySelector("#pic1").style.width = "100%";
+	document.querySelector("#pic2").style.display = "none";
+	document.querySelector("#pic3").style.display = "none";
+	document.querySelector("#pic4").style.display = "none";
+	document.querySelector("#pic5").style.display = "none";
+	document.querySelector("#pic6").style.display = "none";
+	document.querySelector("#pic7").style.display = "none";
+	document.querySelector("#pic8").style.display = "none";
+}
+
+function showPic2(){
+	document.querySelector("#pic2").style.display = "block";
+	document.querySelector("#pic2").style.height = "100%";
+	// document.querySelector("#pic2").style.width = "100%";
+	document.querySelector("#pic1").style.display = "none";
+	document.querySelector("#pic3").style.display = "none";
+	document.querySelector("#pic4").style.display = "none";
+	document.querySelector("#pic5").style.display = "none";
+	document.querySelector("#pic6").style.display = "none";
+	document.querySelector("#pic7").style.display = "none";
+	document.querySelector("#pic8").style.display = "none";
+}
+
+function showPic3(){
+	document.querySelector("#pic3").style.display = "block";
+	document.querySelector("#pic3").style.height = "100%";
+	// document.querySelector("#pic3").style.width = "100%";
+	document.querySelector("#pic1").style.display = "none";
+	document.querySelector("#pic2").style.display = "none";
+	document.querySelector("#pic4").style.display = "none";
+	document.querySelector("#pic5").style.display = "none";
+	document.querySelector("#pic6").style.display = "none";
+	document.querySelector("#pic7").style.display = "none";
+	document.querySelector("#pic8").style.display = "none";
+}
+
+function showPic4(){
+	document.querySelector("#pic4").style.display = "block";
+	document.querySelector("#pic4").style.height = "100%";
+	// document.querySelector("#pic4").style.width = "100%";
+	document.querySelector("#pic1").style.display = "none";
+	document.querySelector("#pic2").style.display = "none";
+	document.querySelector("#pic3").style.display = "none";
+	document.querySelector("#pic5").style.display = "none";
+	document.querySelector("#pic6").style.display = "none";
+	document.querySelector("#pic7").style.display = "none";
+	document.querySelector("#pic8").style.display = "none";
+}
+function showPic5(){
+	document.querySelector("#pic5").style.display = "block";
+	document.querySelector("#pic5").style.height = "100%";
+	// document.querySelector("#pic5").style.width = "100%";
+	document.querySelector("#pic1").style.display = "none";
+	document.querySelector("#pic2").style.display = "none";
+	document.querySelector("#pic3").style.display = "none";
+	document.querySelector("#pic4").style.display = "none";
+	document.querySelector("#pic6").style.display = "none";
+	document.querySelector("#pic7").style.display = "none";
+	document.querySelector("#pic8").style.display = "none";
+}
+
+function showPic6(){
+	document.querySelector("#pic6").style.display = "block";
+	document.querySelector("#pic6").style.height = "100%";
+	// document.querySelector("#pic6").style.width = "100%";
+	document.querySelector("#pic1").style.display = "none";
+	document.querySelector("#pic2").style.display = "none";
+	document.querySelector("#pic3").style.display = "none";
+	document.querySelector("#pic4").style.display = "none";
+	document.querySelector("#pic5").style.display = "none";
+	document.querySelector("#pic7").style.display = "none";
+	document.querySelector("#pic8").style.display = "none";
+}
+
+function showPic7(){
+	document.querySelector("#pic7").style.display = "block";
+	document.querySelector("#pic7").style.height = "100%";
+	// document.querySelector("#pic7").style.width = "100%";
+	document.querySelector("#pic1").style.display = "none";
+	document.querySelector("#pic2").style.display = "none";
+	document.querySelector("#pic3").style.display = "none";
+	document.querySelector("#pic4").style.display = "none";
+	document.querySelector("#pic5").style.display = "none";
+	document.querySelector("#pic6").style.display = "none";
+	document.querySelector("#pic8").style.display = "none";
+}
+
+function showPic8(){
+	document.querySelector("#pic8").style.display = "block";
+	document.querySelector("#pic8").style.height = "100%";
+	// document.querySelector("#pic8").style.width = "100%";
+	document.querySelector("#pic1").style.display = "none";
+	document.querySelector("#pic2").style.display = "none";
+	document.querySelector("#pic3").style.display = "none";
+	document.querySelector("#pic4").style.display = "none";
+	document.querySelector("#pic5").style.display = "none";
+	document.querySelector("#pic6").style.display = "none";
+	document.querySelector("#pic7").style.display = "none";
+}
